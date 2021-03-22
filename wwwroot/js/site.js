a44ffbb29832b71a888b3abcfae41d5110018b86
Oscar Tettamanti <oscar.tettamanti@atlascopco.com>
2021-03-22 10:45:09 +0100

    Init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function ThisFunction(id, person) {
+
+    // This function rocks
+    // This function rocks
+    
+    function Test(){
+        return 0
+    }
+    
+}
+
+
+
